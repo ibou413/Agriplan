5c6fac8f97e5974ce243dc2148ec5cb394747a59
ibou413 <mahdiajf@gmail.com>
2025-08-25 13:31:57 +0200

    deploiement avec fichier media sur coudinary

--- sukeliagri_back/settings.py
+++ sukeliagri_back/settings.py
@@ -281,8 +281,6 @@ CELERY_TIMEZONE = 'Africa/Dakar'
 # Cloudinary - Django Integration
 
 cloudinary.config(
-    cloud_name= os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
-
+  secure= True
 )
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
